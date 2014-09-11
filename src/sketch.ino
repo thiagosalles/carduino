ce137f2d9af538a07b56d474b3733829be174d5c
Thiago Salles <tsmlima@gmail.com>
2014-09-11 00:27:50 -0300

    Configuring the motors

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,10 +1,19 @@
 #include <NewPing.h>
+#include <AFMotor.h>
 
+// Distance Sensor Configurations
 #define TRIGGER_PIN  14
 #define ECHO_PIN     15
 #define MAX_DISTANCE 200
 
+// Motor Configurations
+#define SPEED 255
+#define MOTOR_LEFT 3
+#define MOTOR_RIGHT 2
+
 NewPing DistanceSensor(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+AF_DCMotor MotorLeft(MOTOR_LEFT);
+AF_DCMotor MotorRight(MOTOR_RIGHT);
 
 void setup()
 {
