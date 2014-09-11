8d8edca8a2c4e23e6b4ed08e44996bef939dc53c
Thiago Salles <tsmlima@gmail.com>
2014-09-10 23:54:51 -0300

    Including NewPing lib and configuring the distance sensor

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,8 +1,21 @@
+#include <NewPing.h>
+
+#define TRIGGER_PIN  14
+#define ECHO_PIN     15
+#define MAX_DISTANCE 200
+
+NewPing DistanceSensor(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 void setup()
 {
+    Serial.begin(9600);
 }
 
 void loop()
 {
+    unsigned int cm = DistanceSensor.ping_cm();
+    Serial.print("Distance: ");
+    Serial.print(cm);
+    Serial.println("cm");
+    delay(100);
 }
