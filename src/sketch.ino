4edb0259f68c9fcbd42c8ca182705378bb8dc2c7
Thiago Salles <tsmlima@gmail.com>
2015-01-09 18:32:32 -0200

    WiP - Making walk

--- src/sketch.ino
+++ src/sketch.ino
@@ -4,53 +4,63 @@
 // Distance Sensor Configurations
 #define TRIGGER_PIN  14
 #define ECHO_PIN     15
-#define MAX_DISTANCE 200
+#define MAX_DISTANCE 80
+#define MIN_DISTANCE 30
 
 // Motor Configurations
-#define SPEED 255
+#define MAX_SPEED 255
+#define MIN_SPEED 200
 #define MOTOR_LEFT 3
 #define MOTOR_RIGHT 2
 
 NewPing DistanceSensor(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
-AF_DCMotor MotorLeft(MOTOR_LEFT);
-AF_DCMotor MotorRight(MOTOR_RIGHT);
+AF_DCMotor MotorLeft(MOTOR_LEFT, MOTOR12_1KHZ);
+AF_DCMotor MotorRight(MOTOR_RIGHT, MOTOR12_1KHZ);
 
-void setup()
-{
-    Serial.begin(9600);
+// Car variables
+int speed;
+bool turn;
+
+void setup() {
+	Serial.begin(9600);
+	speed = 0;
+	turn = false;
 }
 
-void loop()
-{
+void loop() {
     unsigned int cm = DistanceSensor.ping_cm();
     Serial.print("Distance: ");
     Serial.print(cm);
     Serial.println("cm");
     delay(100);
 
-	MotorLeft.setSpeed(SPEED);
-	MotorRight.setSpeed(SPEED);
+	turn = false;
+	if(cm == 0) {
+		speed = MAX_SPEED;
+	} else if(cm > MIN_DISTANCE) {
+		speed = MIN_SPEED;
+	} else {
+		speed = MAX_SPEED;
+		turn = true;
+	}
 
-	MotorLeft.run(FORWARD);
-	MotorRight.run(FORWARD);
-	delay(1000);
+	MotorLeft.setSpeed(speed);
+	MotorRight.setSpeed(speed);
 
-	MotorLeft.run(BACKWARD);
-	MotorRight.run(BACKWARD);
-	delay(1000);
-
-	MotorLeft.run(BACKWARD);
-	MotorRight.run(FORWARD);
-	delay(1000);
-
-	MotorLeft.run(FORWARD);
-	MotorRight.run(BACKWARD);
-	delay(1000);
-
-	MotorLeft.setSpeed(0);
-	MotorRight.setSpeed(0);
-
-	MotorLeft.run(BRAKE);
-	MotorRight.run(BRAKE);
-	delay(1000);
+	if(speed > 0) {
+		MotorLeft.run(FORWARD);
+		MotorRight.run(FORWARD);
+	} else if(turn) {
+		MotorLeft.setSpeed(0);
+		MotorRight.setSpeed(0);
+		MotorLeft.run(BRAKE);
+		MotorRight.run(BRAKE);
+	MotorLeft.setSpeed(speed);
+	MotorRight.setSpeed(speed);
+		MotorLeft.run(FORWARD);
+		MotorRight.run(BACKWARD);
+	} else {
+		MotorLeft.run(BRAKE);
+		MotorRight.run(BRAKE);
+	}
 }
